1222cbd8a54f5603b8844e4da8f8d2cfc7b85afc
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-07-12 11:15:39 +0100

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,16 +1,14 @@
-/**
- * Changes the quantity of unique grocery items to 100.
- * @param {Map<String, number>} map - A map of the name of a
- * grocery and its quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
+  const list = map;
+
+  if (list instanceof Map) {
+    for (const [key, value] of list) {
+      if (value === 1) {
+        list.set(key, 100);
+      }
+    }
+  } else {
     throw new Error('Cannot process');
   }
-  map.forEach((value, key) => {
-    if (value === 1) {
-      map.set(key, 100);
-    }
-  });
+  return list;
 }
