5d56a3d6588136450b5f4246f02a156a055ea38d
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-07-12 16:08:52 +0100

    Update crud.js

--- 0x04-TypeScript/task_3/js/crud.js
+++ 0x04-TypeScript/task_3/js/crud.js
@@ -1,5 +1,15 @@
-import { RowID, RowElement } from "./interface";
+export function insertRow(row) {
+  console.log("Insert row", row);
+  return Math.floor(Math.random() * Math.floor(1000));
+}
 
-export declare function insertRow(row: RowElement): RowID;
-export declare function deleteRow(rowId: RowID): void;
-export declare function updateRow(rowId: RowID, row: RowElement): RowID;
+export function deleteRow(rowId) {
+  console.log("Delete row id", rowId);
+  return;
+}
+
+export function updateRow(rowId, row) {
+  console.log(`Update row ${rowId}`, row);
+
+  return rowId;
+}
