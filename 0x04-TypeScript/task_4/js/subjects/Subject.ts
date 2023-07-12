5c0c88d130228a2c83934a73bf57e08904e945d0
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-07-12 16:15:19 +0100

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,1 +1,11 @@
-Didi
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export class Subject {
+    teacher: Subjects.Teacher;
+
+    set setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
